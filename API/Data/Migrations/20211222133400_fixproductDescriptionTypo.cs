ff668583af11b4adf2e002bdb7c2aed70a51c8a9
moshe giterman <moishg@gmail.com>
2021-12-22 17:04:06 +0200

    end of section 2

---
+++ API/Data/Migrations/20211222133400_fixproductDescriptionTypo.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    public partial class fixproductDescriptionTypo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "QuantityStock",
+                table: "Products",
+                newName: "QuantityInStock");
+
+            migrationBuilder.RenameColumn(
+                name: "Descrption",
+                table: "Products",
+                newName: "Description");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "QuantityInStock",
+                table: "Products",
+                newName: "QuantityStock");
+
+            migrationBuilder.RenameColumn(
+                name: "Description",
+                table: "Products",
+                newName: "Descrption");
+        }
+    }
+}
