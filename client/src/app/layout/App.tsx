4304f18f56afdbce8888f189bbb4d3d36c32cce4
moishg <moishg@gmail.com>
2021-12-26 08:05:55 +0200

    chekklenge

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,22 +1,25 @@
 import { ThemeProvider } from "@emotion/react";
 import { Container, createTheme, CssBaseline, Typography } from "@mui/material"; 
+import { useState } from "react";
 import Catalog from "../../features/catalog/Catalog"; 
 import Header from "./Header";
+
 
 function App() {    
   const [darkMode,setDarkMode]=useState(false);
   const paletteType=darkMode ? 'dark' : 'light'
   const theme=createTheme({
     palette:{
-      mode:'dark'
+      mode:paletteType
     }
-
   })
+
+   
   return (
     <>
     <ThemeProvider theme={theme} >
         <CssBaseline />
-        <Header />
+        <Header   />
         <Container>
           <Catalog   />
         </Container>
--- client/src/app/layout/Header.tsx
+++ client/src/app/layout/Header.tsx
@@ -1,11 +1,17 @@
-import { AppBar, Toolbar, Typography } from "@mui/material";
+import { AppBar, Switch, Toolbar, Typography } from "@mui/material";
 
-export default function Header(){
+
+interface HeaderProps{
+    checked:string;
+}
+
+export default function Header({checked}:HeaderProps ){
     return(
         <AppBar position='static' sx={{mb:4}}>
             <Toolbar>
                 <Typography variant='h6'>
                     RE-STORE
+                    <Switch  onChange={(e)=>{checked=e.target.value}} />
                 </Typography>
             </Toolbar>
         </AppBar>
