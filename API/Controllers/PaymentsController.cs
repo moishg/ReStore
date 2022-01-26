d7cea9d25ffc20e3a790976c54eb951ff888ab5b
moshe giterman <moishg@gmail.com>
2022-01-26 11:27:24 +0200

    finished payments -section 11

--- API/Controllers/PaymentsController.cs
+++ API/Controllers/PaymentsController.cs
@@ -2,16 +2,20 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Text.Json.Serialization.Metadata;
 using System.Threading.Tasks;
 using API.Data;
 using API.DTOs;
 using API.Entities;
+using API.Entities.OrderAggregate;
 using API.Extentions;
 using API.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Stripe;
 
@@ -21,8 +25,10 @@ namespace API.Controllers
     {
         private readonly PaymentService _paymentService;
         private readonly StoreContext _context;
-        public PaymentsController(PaymentService paymentService, StoreContext context)
+        private readonly IConfiguration _config;
+        public PaymentsController(PaymentService paymentService, StoreContext context, IConfiguration config)
         {
+            _config = config;
             _context = context;
             _paymentService = paymentService;
         }
@@ -62,9 +68,29 @@ namespace API.Controllers
             basketResult = basket.MapBasketToDto();
 
             return basketResult;
-
-
         }
 
+        [HttpPost("webhook")]
+        public async Task<ActionResult> StripeWebhook()
+        {
+            string json = await new StreamReader(HttpContext.Request.Body).ReadToEndAsync();
+
+            var stripeEvent = EventUtility.ConstructEvent(json, Request.Headers["Stripe-Signature"],
+            _config["StripeSettings:WhSecret"],throwOnApiVersionMismatch:false);
+
+            Charge charge = (Charge)stripeEvent.Data.Object;
+
+            Entities.OrderAggregate.Order order = await _context.Orders.
+                                                FirstOrDefaultAsync(x => x.PaymentIntentId == charge.PaymentIntentId);
+
+            if (charge.Status == "succeeded")
+            {
+                order.OrderStatus = OrderStatus.paymentReceived;
+            }
+
+            await _context.SaveChangesAsync();
+
+            return new EmptyResult();//to inform the stripe we received the request
+        }
     }
 }
